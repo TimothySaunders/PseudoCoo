b1b19c5dc8429c752faf72a9819686756dc2897a
Jonny Reading <jonnyreading@gmail.com>
2020-10-16 21:04:23 +0100

    usually removes grid from processed image. highly depends on quality of input image

--- backEnd/src/main/java/ProcessImage.java
+++ backEnd/src/main/java/ProcessImage.java
@@ -1,3 +1,7 @@
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.Pointer;
+import org.bytedeco.javacpp.indexer.UByteBufferIndexer;
+import org.bytedeco.javacpp.indexer.UByteRawIndexer;
 import org.bytedeco.javacv.*;
 
 import org.bytedeco.opencv.opencv_core.*;
@@ -7,16 +11,43 @@ import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Vector;
 
 import static org.bytedeco.opencv.global.opencv_core.*;
 import static org.bytedeco.opencv.global.opencv_imgproc.*;
 import static org.bytedeco.opencv.global.opencv_imgcodecs.*;
+
+
 public class ProcessImage {
 
     public static void main(String[] args) {
 
         // Read an image. (grayscale)
-        Mat sudoku = imread("/Users/Jonny/codeclan/week_14_react_&&_project/project/PseudoCoo/backEnd/images/sudoku.jpg", 0);
+        Mat imported = imread("images/sudoku4.jpg", 0);
+        Mat sudoku = new Mat();
+        // resize large iamge
+        int wid = imported.cols();
+        int hei = imported.rows();
+        int bigger = max(wid, hei);
+        if (bigger > 800) {
+            int newX;
+            int newY;
+            if (bigger == wid) {
+                newX = 800;
+                double scale = 800 / (double) wid;
+                newY = (int)(hei * scale);
+            } else {
+                newY = 800;
+                double scale = 800 / (double) hei;
+                newX = (int)(wid * scale);
+            }
+            resize(imported, sudoku, new Size(newX, newY), 0, 0, INTER_AREA);
+        } else {
+            sudoku = imported.clone();
+        }
+
+        System.out.println(sudoku.cols());
+
         // show it
         display(sudoku, "original");
 
@@ -24,7 +55,7 @@ public class ProcessImage {
         Mat outerBox = new Mat(sudoku.size(), CV_8UC1);
 
         // run a gaussian blur to filter out noise
-        GaussianBlur(sudoku, sudoku, new Size(11, 11), 0);
+        GaussianBlur(sudoku, sudoku, new Size(13, 13), 0);
 
         // run adaptive threshold to improve contrast
         adaptiveThreshold(sudoku, outerBox, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 3, 2);
@@ -36,22 +67,139 @@ public class ProcessImage {
         Mat kernel = Mat.ones(3, 3, CV_32F).asMat();
         dilate(outerBox, outerBox, kernel);
 
-        // warp perspective
-        outerBox = warpPerspectivePuzzle(outerBox);
+        Mat alt = outerBox.clone();
+//        dilate(alt, alt, kernel);
 
         // invert back to black on white
-        bitwise_not(outerBox, outerBox);
+//        bitwise_not(outerBox, outerBox);
+
+// warp perspective
+//        outerBox = warpPerspectivePuzzle(outerBox);
+
+//        erode(outerBox, outerBox, kernel);
+
+
+// use floodfill to get rid of the gridlines
+
+        int count=0;
+        int max=-1;
+
+        Point maxPt = new Point(0, 0);
+
+        UByteRawIndexer sI = outerBox.createIndexer();
+
+        for (int y = 0; y < outerBox.rows(); y++) {
+
+            for (int x = 0; x < outerBox.cols(); x++) {
+                if (sI.get(y, x) > 200) {
+                    int area = floodFill(alt, new Point(x, y), Scalar.GRAY);
+                    if (area > max) {
+                        maxPt = new Point(x, y);
+                        max = area;
+                    }
+                }
+            }
+        }
+        System.out.println("x: " + maxPt.x());
+        System.out.println("y: " + maxPt.y());
+        floodFill(alt, maxPt, Scalar.WHITE);
+
+        sI = alt.createIndexer();
+        for (int y = 0; y < outerBox.rows(); y++) {
+
+            for (int x = 0; x < outerBox.cols(); x++) {
+                if (sI.get(y, x) < 255 && sI.get(y, x) > 0) {
+                    int area = floodFill(alt, new Point(x, y), Scalar.BLACK);
+                }
+            }
+        }
+
+
+        alt = warpPerspectivePuzzle(alt, outerBox, maxPt);
+        sI = alt.createIndexer();
+        for (int x = 0; x < alt.cols(); x++) {
+            for (int y = 0; y < alt.rows(); y++) {
+                if (x == 0 || x == alt.cols() - 1) {
+                    if (sI.get(y, x) < 200) {
+                        System.out.println("x: " + x + "   y: " + y);
+                        floodFill(alt, new Point(x, y), Scalar.BLACK);
+                    }
+                }
+                if (y == 0 || y == alt.rows() - 1) {
+                    if (sI.get(y, x) < 200) {
+                        System.out.println("x: " + x + "   y: " + y);
+                        floodFill(alt, new Point(x, y), Scalar.BLACK);
+                    }
+                }
+            }
+        }
+
+//        sI = alt.createIndexer();
+//        for (int x = 0; x < 10; x++) {
+//            for (int y = 0; y < 10; y++) {
+//                floodFill(alt, new Point(x, y), Scalar.BLACK);
+//                floodFill(alt, new Point(x, alt.rows() - 1 - y), Scalar.BLACK);
+//                floodFill(alt, new Point(alt.cols() - 1 - x, y), Scalar.BLACK);
+//                floodFill(alt, new Point(alt.cols() - 1 - x, alt.rows() - 1 - y), Scalar.BLACK);
+//            }
+//        }
+
+        erode(alt, alt, kernel);
+
+
+
+        erode(outerBox, outerBox, kernel);
+
+//        CvSeq lines = new CvSeq();
+//        CvMemStorage storage = cvCreateMemStorage(0);
+//        cvHoughLines2(alt, lines, 1, CV_PI/180, 200);
+//
+//        for(int i = 0; i < lines.total(); i++)
+//        {
+//            CvPoint v = new CvPoint(cvGetSeqElem(lines, i));
+////            cvDrawLine(v, outerBox, Scalar.RED);
+//            Pointer line = cvGetSeqElem(lines, i);
+//            CvPoint pt  = new CvPoint(line).position(0);
+//            System.out.println("co-ordinate of  point "+ i + " : "+ pt);
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        for (int i = 0; i < outerBox.cols(); i++) {
+//            floodFill(outerBox, new Point(i, 1), Scalar.WHITE);
+//        }
+//        dilate(outerBox, outerBox, kernel);
+//        erode(outerBox, outerBox, kernel);
+
+//        floodFill(outerBox, new Point(0, 0), Scalar.BLACK);
+
+
+//        MatVector contours = new MatVector();
+//        findContours(alt, contours, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, new Point(0, 0));
+//        for (int i = 0; i < contours.size(); i++) {
+//            System.out.println(contours.get(i).);
+//        }
+
+
+
 
         // display the processed image
+        bitwise_not(alt, alt);
+        display(alt, "alternative");
         display(outerBox, "extracted");
-        
-
-
-
-
-
-
-
+        imwrite("my_image.png", alt);
 
         // convert to black and white
 
@@ -117,7 +265,7 @@ public class ProcessImage {
 //        display(lap, "Laplacian");
     }
     private static Mat warpPerspectiveGrid(Mat image, Point2f corners) {
-        image = deskewImage(image);
+//        image = deskewImage(image);
         Rect rect = getLargestRect(image);
 //        Point2f srcPts = new Point2f(4);
 //        srcPts.position(0).x((float) rect.x()).y((float) rect.y());
@@ -127,20 +275,21 @@ public class ProcessImage {
 
         Point2f dstPts = new Point2f(4);
         dstPts.position(0).x(0).y(0);
-        dstPts.position(1).x(600 - 1).y(0);
-        dstPts.position(2).x(600 - 1).y(600 - 1);
-        dstPts.position(3).x(0).y(600 - 1);
+        dstPts.position(1).x(500 - 1).y(0);
+        dstPts.position(2).x(500 - 1).y(500 - 1);
+        dstPts.position(3).x(0).y(500 - 1);
 
         Mat p = getPerspectiveTransform(corners.position(0), dstPts.position(0));
-        Mat img = new Mat(new Size(600, 600), image.type());//image.size()
+        Mat img = new Mat(new Size(500, 500), image.type());//image.size()
         warpPerspective(image, img, p, img.size());
         return img;
     }
 
 
 
-    private static Mat warpPerspectivePuzzle(Mat image) {
+    private static Mat warpPerspectivePuzzle(Mat image, Mat output, Point gridPoint) {
         image = deskewImage(image);
+        output = deskewImage(output);
         Rect rect = getLargestRect(image);
         Point2f srcPts = new Point2f(4);
         srcPts.position(0).x((float) rect.x()).y((float) rect.y());
@@ -150,13 +299,15 @@ public class ProcessImage {
 
         Point2f dstPts = new Point2f(4);
         dstPts.position(0).x(0).y(0);
-        dstPts.position(1).x(600 - 1).y(0);
-        dstPts.position(2).x(600 - 1).y(600 - 1);
-        dstPts.position(3).x(0).y(600 - 1);
+        dstPts.position(1).x(500 - 1).y(0);
+        dstPts.position(2).x(500 - 1).y(500 - 1);
+        dstPts.position(3).x(0).y(500 - 1);
+
+        floodFill(output, gridPoint, Scalar.BLACK);
 
         Mat p = getPerspectiveTransform(srcPts.position(0), dstPts.position(0));
-        Mat img = new Mat(new Size(600, 600), image.type());//image.size()
-        warpPerspective(image, img, p, img.size());
+        Mat img = new Mat(new Size(500, 500), image.type());//image.size()
+        warpPerspective(output, img, p, img.size());
         return img;
     }
     private static Mat deskewImage(Mat img) {
@@ -180,9 +331,9 @@ public class ProcessImage {
                 angle = -angle;
             }
             Mat rot = getRotationMatrix2D(minAreaRect.center(), angle, 1);
-            Mat dst = new Mat(img.size(), img.type());
-            warpAffine(img, dst, rot, dst.size(), WARP_INVERSE_MAP | INTER_LINEAR, 0, new Scalar(0, 0, 0, 0));
-            return dst;
+//            Mat dst = new Mat(img.size(), img.type());
+            warpAffine(img, img, rot, img.size(), WARP_INVERSE_MAP | INTER_LINEAR, 0, new Scalar(0, 0, 0, 0));
+            return img;
         }
     }
 
@@ -326,6 +477,13 @@ assume that it has a large square with area > 40000*/
         }
         Double d = Collections.max(areas);
         return d > 40000;
+    }
+
+    private static int max(int a, int b) {
+        if (a >= b) {
+            return a;
+        }
+        return b;
     }
     //---------------------------------------------------------------------------
 
