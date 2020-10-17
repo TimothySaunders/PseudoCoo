bc7687c3bf943a8a58c1c7173c186654f8e52bf7
Jonny Reading <jonnyreading@gmail.com>
2020-10-17 11:28:34 +0100

    added more functions to convert between different formats

--- frontEnd/src/helpers/StringParser.js
+++ frontEnd/src/helpers/StringParser.js
@@ -1,16 +1,17 @@
 
 // example string:
 const basicString = "003020600900305001001806400008102900700000008006708200002609500800203009005010300";
+const formatString = "0|e|512#0|e|12#3|n|#0|e|#2|n|#0|e|#6|n|#0|e|#0|e|#9|n|#0|e|#0|e|#3|n|#0|e|#5|n|#0|e|#0|e|#1|n|#0|e|#0|e|#1|n|#8|n|#0|e|#6|n|#4|n|#0|e|#0|e|#0|e|#0|e|#8|n|#1|n|#0|e|#2|n|#9|n|#0|e|#0|e|#7|n|#0|e|#0|e|#0|e|#0|e|#0|e|#0|e|#0|e|#8|n|#0|e|#0|e|#6|n|#7|n|#0|e|#8|n|#2|n|#0|e|#0|e|#0|e|#0|e|#2|n|#6|n|#0|e|#9|n|#5|n|#0|e|#0|e|#8|n|#0|e|#0|e|#2|n|#0|e|#3|n|#0|e|#0|e|#9|n|#0|e|#0|e|#5|n|#0|e|#1|n|#0|e|#3|n|#0|e|#0|e|#";
 
 /**
  * takes an 81 char input string and converts to cell array
  * @param {str} inputString
  */
-const formatStringToCells = function (inputString) {
-    const regex = /([A-Za-z])+/;
+const formatRawStringToCells = function (inputString) {
     // make sure you've got a correct length string
     if (inputString.length === 81) {
         // make sure you've got only numbers present
+        const regex = /([a-z])+/gi;
         if (!inputString.match(regex)) {
             let chunks = inputString.split("");
             let output = [];
@@ -36,14 +37,88 @@ const formatStringToCells = function (inputString) {
     }
 }
 
+/**
+ * takes the output from formatRawStringToCells and converts to a string
+ * @param {array} inputArray 
+ */
 const formatCellsToString = function (inputArray) {
     let outputString = inputArray.join("");
     return outputString;
+}
+
+/**
+ * takes a formatted input string from formatCellsToString and returns an array of cell objects
+ * @param {string} parsedInputString 
+ */
+const getObjectsFromSavedString = function (parsedInputString) {
+    const split = parsedInputString.split("#");
+    const cellObjects = [];
+    split.pop(); //removes the last element - it was blank. length now 81
+    split.forEach(cell => {
+        let cellObj = {};
+        let pieces = cell.split("|");
+        cellObj.value = pieces[0];
+        cellObj.editable = pieces[1] === "e" ? true : false;
+        cellObj.notes = pieces[2].split("");
+        cellObjects.push(cellObj);
+    });
+    console.log(cellObjects);
+    return cellObjects;
+}
+
+/**
+ * takes the object array from getObjectsFromSavedString and returns a formatted string
+ * @param {array[{}]} inputObjectArray 
+ */
+const convertObjectsToSaveString = function (inputObjectArray) {
+    let saveString = "";
+    inputObjectArray.forEach(cell => {
+        let value = cell.value;
+        let editable = (cell.editable ? "e" : "n");
+        let notes = cell.notes.join("");
+        saveString += value + "|" + editable + "|" + notes + "#";
+    });
+    return saveString;
+}
+
+/**
+ * output an 81 char string from an array of cell objects
+ * @param {array[{}]} inputObjectArray 
+ */
+const getRawStringFromObjects = function (inputObjectArray) {
+    let outputString = "";
+    inputObjectArray.forEach(cell => {
+        outputString += cell.value;
+    });
+    return outputString;
+}
+
+/**
+ * output an 81 char string from a formatted cells string
+ * @param {string} inputCellsString 
+ */
+const getRawStringFromCells = function (inputCellsString) {
+    const split = inputCellsString.split("#");
+    split.pop(); //removes the last element - it was blank. length now 81
+    let outputString = "";
+    split.forEach(cell => {
+        outputString += cell[0];
+    });
+    console.log(outputString);
 
 }
 
 
-// formatCellsToString(parsed);
-// let foo = parsed.join("");
+
+// let parsed = formatRawStringToCells(basicString);
+// parsed = formatCellsToString(parsed);
+// getRawStringFromCells(parsed);
+
+
+// let foo = formatCellsToString(parsed);
+// // let foo = parsed.join("");
+// // console.log(foo);
+// let bar = getObjectsFromSavedString(formatString);
 // console.log(foo);
+// console.log(convertObjectsToSaveString(bar));
 
