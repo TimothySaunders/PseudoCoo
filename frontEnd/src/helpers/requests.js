06a728b77879ea3e26e242b65a11406e5fd97045
Jonny Reading <jonnyreading@gmail.com>
2020-10-17 22:13:51 +0100

    merge requests.js

--- frontEnd/src/helpers/requests.js
+++ frontEnd/src/helpers/requests.js
@@ -1,10 +1,17 @@
-export default function uploadImage(file) {
-    const fd = new FormData();
-    fd.append('avatar', file);
-    fetch("api/images", {
-        method: "POST",
-        // headers: {"Content-Type": "image/jpeg"},
-        body: fd
-    })
-    .then(res => console.log(res));
-}
+
+    export function uploadImage(file) {
+        fetch("api/images", {
+            method: "POST",
+            headers: {"Content-Type": "image/jpg"},
+            body: file
+        })
+        .then(res => console.log(res));
+    }
+
+    export function getSaves(){
+        fetch("api/saves")
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
+
