b812fae1df5e11174c3eaedb71bf38d662922bd8
Timothy Saunders <timsaunders88@gmail.com>
2020-10-17 14:45:55 +0100

    TS 17/10/20 frontend - added ImageUpload component basic structure

---
+++ frontEnd/src/containers/ImageUpload.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export default class ImageUpload extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            imageFile: null
+        }
+    }
+
+    render(){
+        return(
+            
+        )
+    }
+
+
+}
