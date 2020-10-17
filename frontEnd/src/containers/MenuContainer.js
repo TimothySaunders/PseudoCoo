5998c55a212a6290df5b637dd01dcb693e9b7acc
marekboro <marek.borowicz83@gmail.com>
2020-10-17 11:27:47 +0100

    buinding structure

---
+++ frontEnd/src/containers/MenuContainer.js
@@ -0,0 +1,52 @@
+import React, {Component, Fragment} from 'react'
+import MenuView from '../components/MenuView'
+
+
+export default class MenuContainer extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            gameString:"",
+            viewOption: "mainMenu",
+            savedGames: []
+        }
+
+        this.newGame=this.newGame.bind(this);
+        this.importImage=this.importImage.bind(this);
+        this.viewSavedGames=this.viewSavedGames.bind(this);
+        this.chooseMenu=this.chooseMenu.bind(this);
+
+    };
+    
+    newGame(){
+        this.setState({viewOption:"new"})
+    }
+    importImage(){
+        this.setState({viewOption:"importImage"})
+    }
+    viewSavedGames(){
+        this.setState({viewOption:"savedGames"})
+    }
+    chooseMenu(choice){
+        this.setState({viewOption:choice})
+
+    }
+
+
+
+    render(){
+        return(
+            <Fragment>
+                I am the menus container.
+                {/* <br/>
+                <button onClick={this.newGame} value="newGame"> Play </button><br/>
+                <button onClick={this.importImage}> Extract from Image </button><br/>
+                <button onClick={this.viewSavedGames}> Continue game </button> */}
+                <MenuView chooseMenu={this.chooseMenu}> </MenuView>
+
+            </Fragment>
+        )
+    }
+
+
+}
