5171f8d5316097ea23b1e81c0ec215ec73267b9b
Jonny Reading <jonnyreading@gmail.com>
2020-10-20 13:34:47 +0100

    cow-ified the background, broke out loag-game-item into a new component

---
+++ frontEnd/src/components/LoadGameItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./LoadGameItem.css";
+
+export default function LoadGameItem({ game, clickEvent, deleteEvent }) {
+    const handleLoadGame = (event) => {
+        clickEvent(event);
+    }
+    const handleDeleteGame = (event) => {
+        event.stopPropagation();
+        deleteEvent(event);
+    }
+    return (
+        <section id={`save-${game.id}`} className="load-game-item" onClick={handleLoadGame}>
+            <span>{game.timeStamp}</span>
+            <button id={"del-" + game.id} className="delete" onClick={handleDeleteGame}><span className="fas fa-exclamation-triangle"></span> Delete</button>
+        </section>
+    )
+}
